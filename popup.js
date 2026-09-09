3f979c32fe155212f95d8e19f4aac5ef902858c3
Elio El Hage <el.hage.elio@umontreal.ca>
2026-09-09 19:50:35 -0400

    Bump Better Crunchyroll popup version to v2.15

--- popup.js
+++ popup.js
@@ -1,5 +1,5 @@
 const STORAGE_KEY = 'betterCrunchyrollEnabled';
-const VERSION = '0.2.14';
+const VERSION = '0.2.15';
 
 const popupElement = document.querySelector('.popup');
 const toggleButton = document.getElementById('toggleButton');
