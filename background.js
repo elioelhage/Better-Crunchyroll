aa991159993afda2e72dbe6d1c451dd7f592442f
Elio El Hage <el.hage.elio@umontreal.ca>
2026-09-09 19:00:54 -0400

    Update background version to v2.13

--- background.js
+++ background.js
@@ -1,5 +1,5 @@
 const STORAGE_KEY = 'betterCrunchyrollEnabled';
-const VERSION = '0.2.12';
+const VERSION = '0.2.13';
 
 chrome.runtime.onInstalled.addListener(async () => {
   console.log('Better Crunchyroll installed.');
