380e4e9afb2ddd3fedae26ed6ad0a9b32011e9ca
Elio El Hage <el.hage.elio@umontreal.ca>
2026-09-09 20:41:05 -0400

    Sync popup logo state and version for v2.17

--- popup.js
+++ popup.js
@@ -1,5 +1,5 @@
 const STORAGE_KEY = 'betterCrunchyrollEnabled';
-const VERSION = '0.2.16';
+const VERSION = '0.2.17';
 
 const popupElement = document.querySelector('.popup');
 const toggleButton = document.getElementById('toggleButton');
